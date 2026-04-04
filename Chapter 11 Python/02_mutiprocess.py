6e32c10c281e0909377486259985901492bf95d5
Paras-Gith <161969750+Paras-Gith@users.noreply.github.com>
2026-04-04 19:23:27 +0530

    Add files via upload

---
+++ Chapter 11 Python/02_mutiprocess.py
@@ -0,0 +1,26 @@
+from multiprocessing import Process
+import time
+
+def brew_chai(name):
+    print(f"{name} chai served")
+    time.sleep(3)
+    print(f"End of {name} chai brewing")
+
+if __name__ == "__main__":
+    chai_makers = [
+        Process(target=brew_chai, args=(f"chai maker #{i + 1}", ))
+        for i in range(3)
+    ]    
+
+#start all process
+    for p in chai_makers:
+        p.start()
+
+
+#wait for all to complete
+  
+    for p in chai_makers:
+        p.join()
+
+    print("all chai served!!")
+
