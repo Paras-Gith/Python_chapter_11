194664d34197a0e265a8b1a9a8c2a64d0c56d703
Paras-Gith <161969750+Paras-Gith@users.noreply.github.com>
2026-04-09 20:13:53 +0530

    Add files via upload

---
+++ Chapter 11 Python/11_process_qureue.py
@@ -0,0 +1,13 @@
+from multiprocessing import Process, Queue, Value
+
+def prepare_chai(queue):
+    queue.put("Masala chai is readay")
+
+
+
+if __name__ == '__main__':
+    queue =  Queue()
+    p = Process(target= prepare_chai, args=(queue,))
+    p.start()
+    p.join()
+    print(queue.get())
